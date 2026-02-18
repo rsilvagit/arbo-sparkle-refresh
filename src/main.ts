565b0873f3bd121ae62c49b4cf86d18994c67364
Rafael Silva <rafael.sdasilva84@hotmail.com>
2026-02-18 09:56:37 -0300

    feat: seo review

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { initNavbar } from './modules/navbar';
 import { initScrollAnimator } from './modules/scroll-animator';
 import { initContactModal } from './modules/contact-modal';
 import { initFooter } from './modules/footer';
+import { initCookieConsent } from './modules/cookie-consent';
 
 document.addEventListener('DOMContentLoaded', () => {
   initNavbar();
   initScrollAnimator();
   initContactModal();
   initFooter();
+  initCookieConsent();
 });
